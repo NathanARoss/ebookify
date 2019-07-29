645bb38d69a9c5db43dcf2f33c3c6c57e18d28e5
Nathan Ross <NathanAnthonyRoss19@gmail.com>
2019-07-28 23:51:32 -0700

    Allowed users to upload images, added icons

--- main.mjs
+++ main.mjs
@@ -1,60 +1,55 @@
-let streaming = false;
+const video = document.getElementById('video');
+const canvas = document.getElementById('video-frame');
+const context = canvas.getContext('2d');
+const captureButton = document.getElementById('capture-button');
+const uploadButton = document.getElementById('photo-upload');
+const cameraPreview = document.getElementById('camera-preview');
 
-let video = null;
-let canvas = null;
-let startbutton = null;
-let cameraPreview = null;
+let streaming = false;
 let stream = null;
 
-function startup() {
-    video = document.getElementById('video');
-    canvas = document.getElementById('video-frame');
-    startbutton = document.getElementById('capture-button');
-    cameraPreview = document.getElementById('camera-preview');
+navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" }, audio: false })
+    .then(function (_stream) {
+        stream = _stream;
+        video.srcObject = _stream;
+        video.play();
+    })
+    .catch(function (err) {
+        console.log("An error occurred: " + err);
+    });
 
-    navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" }, audio: false })
-        .then(function (_stream) {
-            stream = _stream;
-            video.srcObject = _stream;
-            video.play();
-        })
-        .catch(function (err) {
-            console.log("An error occurred: " + err);
-        });
+video.addEventListener('canplay', function (ev) {
+    if (!streaming) {
+        video.width = video.videoWidth;
+        video.height = video.videoHeight;
+        streaming = true;
+    }
+}, false);
 
-    video.addEventListener('canplay', function (ev) {
-        if (!streaming) {
-            video.width = video.videoWidth;
-            video.height = video.videoHeight;
-            canvas.width = video.videoWidth;
-            canvas.height = video.videoHeight;
-            streaming = true;
-        }
-    }, false);
+captureButton.addEventListener('click', function (ev) {
+    ev.preventDefault();
 
-    startbutton.addEventListener('click', function (ev) {
-        takepicture();
-        ev.preventDefault();
-    }, false);
-
-    clearphoto();
-}
-
-// Fill the photo with an indication that none has been
-// captured.
-
-function clearphoto() {
-    let context = canvas.getContext('2d');
-    context.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-function takepicture() {
     if (streaming) {
-        let context = canvas.getContext('2d');
+        canvas.width = video.width;
+        canvas.height = video.height;
         context.drawImage(video, 0, 0, video.width, video.height);
     } else {
-        clearphoto();
+        context.clearRect(0, 0, canvas.width, canvas.height);
     }
-}
+}, false);
 
-window.addEventListener('load', startup, false);
+uploadButton.onchange = function (ev) {
+    const files = ev.target.files;
+
+    const fileReader = new FileReader();
+    fileReader.onload = function (ev) {
+        var img = new Image();
+        img.onload = function () {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            context.drawImage(img, 0, 0);
+        };
+        img.src = ev.target.result;
+    };
+    fileReader.readAsDataURL(files[0]);
+}
